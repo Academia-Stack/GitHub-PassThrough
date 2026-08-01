aed9c75f8832c76faa5d4286cdf592cf8e94ab89
DarkMortal <dragonbeast.saiyan@gmail.com>
2026-08-01 17:02:43 +0530

    Added interceptor logic

--- src/main/server/request/controller/GitHubController.java
+++ src/main/server/request/controller/GitHubController.java
@@ -2,11 +2,9 @@ package request.controller;
 
 import request.dto.github.GitHubUser;
 import request.dto.github.GitHubUserList;
-import request.dto.server.ResponseDTO;
 import request.services.interfaces.IGitHubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,33 +18,22 @@ public class GitHubController {
     @Autowired
     private IGitHubService githubUserService;
 
-    @GetMapping(path = "/users",
-        produces = { MediaType.APPLICATION_JSON_VALUE })
-    public ResponseEntity<ResponseDTO<List<GitHubUser>>> getAllUsersJSON() {
-        return ResponseEntity.ok(
-            ResponseDTO.<List<GitHubUser>>builder()
-                .data(githubUserService.getAllUsers())
-                .build());
+    @GetMapping(path = "/users", produces = { MediaType.APPLICATION_JSON_VALUE })
+    public List<GitHubUser> getAllUsersJSON() {
+        return githubUserService.getAllUsers();
     }
 
-    @GetMapping(path = "/users",
-        produces = { MediaType.APPLICATION_XML_VALUE })
-    public ResponseEntity<ResponseDTO<GitHubUserList>> getAllUsersXML() {
-        return ResponseEntity.ok(
-            ResponseDTO.<GitHubUserList>builder()
-                .data(GitHubUserList.builder()
-                    .users(githubUserService.getAllUsers())
-                    .build())
-                .build());
+    @GetMapping(path = "/users", produces = { MediaType.APPLICATION_XML_VALUE })
+    public GitHubUserList getAllUsersXML() {
+        return GitHubUserList.builder()
+                .users(githubUserService.getAllUsers())
+                .build();
     }
 
     @GetMapping(path = "/users/{username}", produces = {
-        MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE
+            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE
     })
-    public ResponseEntity<ResponseDTO<GitHubUser>> getUserByUsername(@PathVariable String username) {
-        return ResponseEntity.ok(
-            ResponseDTO.<GitHubUser>builder()
-                .data(githubUserService.getUserByUsername(username))
-                .build());
+    public GitHubUser getUserByUsername(@PathVariable String username) {
+        return githubUserService.getUserByUsername(username);
     }
 }
